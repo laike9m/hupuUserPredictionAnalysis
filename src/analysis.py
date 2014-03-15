ac49284256e469806482d559ec1ca81d2d195471
laike9m <laike9m@gmail.com>
2014-03-15 15:32:59 +0800

    simple analysis done

--- src/analysis.py
+++ src/analysis.py
@@ -28,6 +28,7 @@ from clubdata import club
 
 RESULTS = (1, 3, 3, 3)
 LEAGUE = ('laliga', 'premierleague', 'ligue1', 'bundesliga', 'seriea')
+LEAGUE2 = tuple((x, y) for x in LEAGUE for y in LEAGUE[LEAGUE.index(x) + 1:])
 
 
 class DataAnalysis():
@@ -57,9 +58,60 @@ class DataAnalysis():
             loi = list(set(loi_interest + loi_affiliation))
             self.collection.update({'_id': doc['_id']}, {'$set': {'loi': loi}})
 
+    def calc_level_correlation(self):
+        """计算等级关联,粒度10level,0-9,10-19,20-29,30-39,40+"""
+        stats = [{'hit': 0, 'gap': 0, 'amount': 0} for _ in range(5)]
+        for doc in self.collection.find():
+            temp = int(doc['info']['level']) // 10
+            index = temp if temp < 5 else 4
+            stats[index]['hit'] += int(doc['hit'])
+            stats[index]['gap'] += int(doc['gap'])
+            stats[index]['amount'] += 1
+        from pprint import pprint
+        for d in stats:
+            d['avg_hit'] = round(d['hit'] / d['amount'], 3)
+            d['avg_gap'] = round(d['gap'] / d['amount'], 3)
+        pprint(stats)
+
+    def calc_loi_correlation(self):
+        from pprint import pprint
+        stats = {k: {'hit': 0, 'gap': 0, 'amount': 0} for k in ('null',) + LEAGUE}
+        self.collection.ensure_index('loi')  # this is a multikey index
+        for league in LEAGUE:
+            for doc in self.collection.find({'loi': league}):
+                stats[league]['hit'] += int(doc['hit'])
+                stats[league]['gap'] += int(doc['gap'])
+                stats[league]['amount'] += 1
+        for doc in self.collection.find({'loi': []}):  # people without loi
+            stats['null']['hit'] += int(doc['hit'])
+            stats['null']['gap'] += int(doc['gap'])
+            stats['null']['amount'] += 1
+        for v in stats.values():
+            v['avg_hit'] = round(v['hit'] / v['amount'], 3)
+            v['avg_gap'] = round(v['gap'] / v['amount'], 3)
+
+        stats2 = {k: {'hit': 0, 'gap': 0, 'amount': 0} for k in LEAGUE2}
+        for league2 in LEAGUE2:
+            for doc in self.collection.find({'$and': [{'loi': league2[0]}, {'loi': league2[1]}]}):
+                try:
+                    stats2[league2]['hit'] += int(doc['hit'])
+                    stats2[league2]['gap'] += int(doc['gap'])
+                    stats2[league2]['amount'] += 1
+                except KeyError:
+                    pass
+        for v in stats2.values():
+            v['avg_hit'] = round(v['hit'] / v['amount'], 3)
+            v['avg_gap'] = round(v['gap'] / v['amount'], 3)
+
+        with open('loi_correlation.txt', 'wt') as f:
+            pprint(stats, f)
+            pprint(stats2, f)
+
 
 if __name__ == '__main__':
     analyzer = DataAnalysis()
-    analyzer.evaluate_correctness()
-    analyzer.find_loi()
-    analyzer.readdata()
+    #analyzer.evaluate_correctness()
+    #analyzer.find_loi()
+    #analyzer.readdata()
+    #analyzer.calc_level_correlation()
+    analyzer.calc_loi_correlation()
