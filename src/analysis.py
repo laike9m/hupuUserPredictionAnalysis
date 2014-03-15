8deff8fa8a011b25262539b6079377941a7f9ab6
laike9m <laike9m@gmail.com>
2014-03-15 20:34:24 +0800

    finished

--- src/analysis.py
+++ src/analysis.py
@@ -23,6 +23,8 @@ document示例 {
 (3) 评价等级关联度
 (4) 评价联赛关注关联度
 """
+from collections import OrderedDict
+
 import pymongo
 
 import conf
@@ -62,54 +64,89 @@ class DataAnalysis():
 
     def calc_level_correlation(self):
         """计算等级关联,粒度10level,0-9,10-19,20-29,30-39,40+"""
-        stats = [{'hit': 0, 'gap': 0, 'amount': 0} for _ in range(5)]
+        self.stats_level = [OrderedDict({'hit': 0, 'gap': 0, 'amount': 0}) for _ in range(5)]
         for doc in self.collection.find():
             temp = int(doc['info']['level']) // 10
             index = temp if temp < 5 else 4
-            stats[index]['hit'] += int(doc['hit'])
-            stats[index]['gap'] += int(doc['gap'])
-            stats[index]['amount'] += 1
+            self.stats_level[index]['hit'] += int(doc['hit'])
+            self.stats_level[index]['gap'] += int(doc['gap'])
+            self.stats_level[index]['amount'] += 1
         from pprint import pprint
-        for d in stats:
+        for d in self.stats_level:
             d['avg_hit'] = round(d['hit'] / d['amount'], 3)
             d['avg_gap'] = round(d['gap'] / d['amount'], 3)
         with open('../data/level_correlation_%d.txt' % conf.page_id, 'wt') as f:
-            pprint(stats, f)
+            pprint(self.stats_level, f)
 
     def calc_loi_correlation(self):
         from pprint import pprint
         self.collection.ensure_index('loi')  # this is a multikey index
 
-        stats = {k: {'hit': 0, 'gap': 0, 'amount': 0} for k in ('null',) + LEAGUE}
+        self.stats_loi1 = {k: OrderedDict({'hit': 0, 'gap': 0, 'amount': 0}) for k in ('null',) + LEAGUE}
         for league in LEAGUE:
             for doc in self.collection.find({'loi': league}):
-                stats[league]['hit'] += int(doc['hit'])
-                stats[league]['gap'] += int(doc['gap'])
-                stats[league]['amount'] += 1
+                self.stats_loi1[league]['hit'] += int(doc['hit'])
+                self.stats_loi1[league]['gap'] += int(doc['gap'])
+                self.stats_loi1[league]['amount'] += 1
         for doc in self.collection.find({'loi': []}):  # people without loi
-            stats['null']['hit'] += int(doc['hit'])
-            stats['null']['gap'] += int(doc['gap'])
-            stats['null']['amount'] += 1
-        for v in stats.values():
+            self.stats_loi1['null']['hit'] += int(doc['hit'])
+            self.stats_loi1['null']['gap'] += int(doc['gap'])
+            self.stats_loi1['null']['amount'] += 1
+        for v in self.stats_loi1.values():
             v['avg_hit'] = round(v['hit'] / v['amount'], 3)
             v['avg_gap'] = round(v['gap'] / v['amount'], 3)
 
-        stats2 = {k: {'hit': 0, 'gap': 0, 'amount': 0} for k in LEAGUE2}
+        self.stats_loi2 = {k: OrderedDict({'hit': 0, 'gap': 0, 'amount': 0}) for k in LEAGUE2}
         for league2 in LEAGUE2:
             for doc in self.collection.find({'$and': [{'loi': league2[0]}, {'loi': league2[1]}]}):
                 try:
-                    stats2[league2]['hit'] += int(doc['hit'])
-                    stats2[league2]['gap'] += int(doc['gap'])
-                    stats2[league2]['amount'] += 1
+                    self.stats_loi2[league2]['hit'] += int(doc['hit'])
+                    self.stats_loi2[league2]['gap'] += int(doc['gap'])
+                    self.stats_loi2[league2]['amount'] += 1
                 except KeyError:
                     pass
-        for v in stats2.values():
+        for v in self.stats_loi2.values():
             v['avg_hit'] = round(v['hit'] / v['amount'], 3)
             v['avg_gap'] = round(v['gap'] / v['amount'], 3)
 
         with open('../data/loi_correlation_%d.txt' % conf.page_id, 'wt') as f:
-            pprint(stats, f)
-            pprint(stats2, f)
+            pprint(self.stats_loi1, f)
+            pprint(self.stats_loi2, f)
+
+    def write_to_excel(self):
+        """
+        [{'amount': 267, 'avg_gap': 4.15, 'avg_hit': 2.112, 'gap': 1108, 'hit': 564},
+         {'amount': 168, 'avg_gap': 4.185, 'avg_hit': 2.083, 'gap': 703, 'hit': 350},
+         {'amount': 87, 'avg_gap': 4.241, 'avg_hit': 2.103, 'gap': 369, 'hit': 183},
+         {'amount': 32, 'avg_gap': 3.875, 'avg_hit': 2.156, 'gap': 124, 'hit': 69},
+         {'amount': 18, 'avg_gap': 4.5, 'avg_hit': 2.0, 'gap': 81, 'hit': 36}]
+        """
+        import xlwt
+        book = xlwt.Workbook()
+        sheet1 = book.add_sheet('Sheet 1', cell_overwrite_ok=True)
+        sheet2 = book.add_sheet('Sheet 2', cell_overwrite_ok=True)
+        row10, row20 = sheet1.row(0), sheet2.row(0)
+        # write head
+        for i, key in enumerate(self.stats_level[0], 1):
+            row10.write(i, key)
+            row20.write(i, key)
+        row10.write(0, "等级")
+        row20.write(0, "关注联赛")
+
+        # write sheet 1: level correlation stats
+        for row, element in enumerate(self.stats_level, 1):
+            sheet1.write(row, 0, str(10 * row - 10) + '~' + str(10 * row -1))
+            for col, value in enumerate(element.values(), 1):
+                sheet1.write(row, col, value)
+
+        # write sheet 2: loi correlation stats
+        from itertools import chain
+        for row, (loi, element) in enumerate(chain(self.stats_loi1.items(), self.stats_loi2.items()), 1):
+            sheet2.write(row, 0, str(loi))
+            for col, value in enumerate(element.values(), 1):
+                sheet2.write(row, col, value)
+
+        book.save('../data/analysis_result_%d.xls' % conf.page_id)
 
 
 if __name__ == '__main__':
@@ -119,3 +156,4 @@ if __name__ == '__main__':
     #analyzer.readdata()
     analyzer.calc_level_correlation()
     analyzer.calc_loi_correlation()
+    analyzer.write_to_excel()
